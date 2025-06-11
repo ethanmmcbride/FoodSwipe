b42def1fbb2d923fe0e89985b6c476a484ca7596
Ethan McBride <ethanmmcbride@gmail.com>
2025-06-11 11:35:01 -0700

    Initial Commit

---
+++ tinder/tinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinderApp.swift
+//  tinder
+//
+//  Created by Ethan McBride on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct tinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tinderTests/tinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  tinderTests.swift
+//  tinderTests
+//
+//  Created by Ethan McBride on 6/11/25.
+//
+
+import Testing
+@testable import tinder
+
+struct tinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
